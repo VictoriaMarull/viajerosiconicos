593bc37c1e8a4448035798c24f4cbbfa78241221
VictoriaMarull <vmarull@mail.austral.edu.ar>
2024-11-15 09:26:36 -0300

    Agrego funcion global

---
+++ R/globals.R
@@ -0,0 +1,5 @@
+utils::globalVariables(c(
+  "fecha", "granizo", "horas_frio", "precipitacion_pluviometrica",
+  "temperatura_abrigo_150cm", "temperatura_abrigo_150cm_maxima",
+  "temperatura_abrigo_150cm_minima", "temperatura_intemperie_5cm_minima"
+))
